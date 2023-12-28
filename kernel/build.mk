7a2437c2ee00cda9f2385e50d9db4a9cb627e0e6
RI5255 <riruoda@gmail.com>
2023-12-28 20:44:57 +0900

    Move wasm dir under libs/kernel

--- kernel/build.mk
+++ kernel/build.mk
@@ -1,8 +1,8 @@
 objs-y    += main.o printk.o memory.o task.o interrupt.o ipc.o syscall.o bootelf.o \
              hinavm.o wasmvm.o libgcc.a
-# add "libs/wasm/include" to include path
-cflags-y  += -Ilibs/wasm/include
-libs-y    += wasm
+
+cflags-y  += -Ilibs/kernel/wasm/include
+libs-y    += kernel
 subdirs-y += riscv32
 
 $(build_dir)/bootelf.o: $(boot_elf)
