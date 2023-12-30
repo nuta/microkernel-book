1165fe34b1e5a6a45c8552c24576028c1f633a5e
RI5255 <riruoda@gmail.com>
2023-12-30 21:16:04 +0900

    Use pre-built libgcc.a

--- kernel/build.mk
+++ kernel/build.mk
@@ -1,20 +1,8 @@
 objs-y    += main.o printk.o memory.o task.o interrupt.o ipc.o syscall.o bootelf.o \
-             hinavm.o wasmvm.o libgcc.a
+             hinavm.o wasmvm.o
 
 cflags-y  += -Ilibs/kernel/wasm/include
 libs-y    += kernel
 subdirs-y += riscv32
 
 $(build_dir)/bootelf.o: $(boot_elf)
-
-# WAMR builds require libgcc.a, so use the one from riscv-gnu-toolchain
-# To build from source, execute the following commands
-# $ git clone https://github.com/riscv-collab/riscv-gnu-toolchain
-# $ cd riscv-gnu-toolchain
-# $ ./configure --prefix=/opt/riscv32 --with-arch=rv32imac --with-abi=ilp32 --with-target-cflags=-mno-relax
-# $　sudo make -j$(nproc)
-libgcc_archive := /opt/riscv32/lib/gcc/riscv32-unknown-elf/13.2.0/libgcc.a
-$(build_dir)/libgcc.a:
-	$(PROGRESS) CP $(@)
-	$(MKDIR) -p $(@D)
-	$(CP) $(libgcc_archive) $@
--- kernel/riscv32/build.mk
+++ kernel/riscv32/build.mk
@@ -1,1 +1,11 @@
-objs-y += boot.o setup.o task.o vm.o mp.o switch.o handler.o trap.o usercopy.o debug.o uart.o plic.o
+objs-y += boot.o setup.o task.o vm.o mp.o switch.o handler.o trap.o usercopy.o debug.o uart.o plic.o libgcc.a
+
+# To build WAMR, you will need libgcc.a, so use the one from the riscv-gnu-toolchain(https://github.com/riscv-collab/riscv-gnu-toolchain). 
+# The pre-built version is available at kernel/riscv32/libgcc.a. 
+# If you want to build it from the source code, please follow the instructions in the README. 
+# Use the following command for configuration.
+# $ ./configure --prefix=/opt/riscv32 --with-arch=rv32imac --with-abi=ilp32 --with-target-cflags=-mno-relax
+
+$(build_dir)/libgcc.a: $(dir)/libgcc.a
+	$(PROGRESS) CP $@
+	$(CP) $< $@
